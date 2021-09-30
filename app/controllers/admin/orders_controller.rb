f5fdbca4378dffe8107365688c420279b5445fde
kudan0 <7xxx.ccs@gmail.com>
2021-09-30 05:04:37 +0000

    [add]ジャンル（レイアウト以外）

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,9 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    # @post_images = PostImage.page(params[:page]).reverse_order# ページング
+  end
+
+  def update
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  # deviseのルーティングをadminsからadminへ変更
+  devise_for :admins,
+    path: :admin,
+      :controllers => {
+        :registrations => 'devise/registrations',
+        :sessions => 'devise/sessions',
+        :passwords => 'devise/passwords'
+      }
+
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 管理側ルーティング
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:show, :update]
+    resources :order_items, only:[:update]
+  end
+
 end
