2bb3f4559789edc60ead528b7b1a91d2d088daa2
kudan0 <7xxx.ccs@gmail.com>
2021-10-01 13:19:52 +0000

    [add]item(レイアウト以外)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,9 +19,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genre = Genre.all
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_items_path
   end
 
   private
