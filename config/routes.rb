c593822c2042b25881cf728334dbc8c2a7786876
kudan0 <7xxx.ccs@gmail.com>
2021-10-01 15:38:44 +0000

    [add]publicのルーティングとcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  scope module: :public do
+
+    get "/" => "homes#top"
+    resources :homes, only:[:about]
+    resources :items, only:[:index, :show]
+    resources :customers, only:[:show, :edit, :update, :confirm, :destroy]
+    resources :cart_items, only:[:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only:[:new, :confirm, :complete, :create, :index, :show]
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+  end
+
   # deviseのルーティングをadminsからadminへ変更
   devise_for :admins,
     path: :admin,
@@ -7,8 +18,16 @@ Rails.application.routes.draw do
         :sessions => 'devise/sessions',
         :passwords => 'devise/passwords'
       }
-
+  # devise のルーティングをcustomersからpublicへ変更
   devise_for :customers
+    # path: :public,
+    #   :controllers => {
+    #     :registrations => 'devise/registrations',
+    #     :sessions => 'devise/sessions',
+    #     :passwords => 'devise/passwords'
+    #   }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 管理側ルーティング
