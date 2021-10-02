1a9f7fd88af461b2913c26cbbfb85a859a6e1e85
kudan0 <7xxx.ccs@gmail.com>
2021-10-02 05:30:02 +0000

    [add]public/homes途中まで

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @genres = Genre.all #ジャンル検索用
+    @item = Item.all #新着商品用
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -20,14 +20,6 @@ Rails.application.routes.draw do
       }
   # devise のルーティングをcustomersからpublicへ変更
   devise_for :customers
-    # path: :public,
-    #   :controllers => {
-    #     :registrations => 'devise/registrations',
-    #     :sessions => 'devise/sessions',
-    #     :passwords => 'devise/passwords'
-    #   }
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 管理側ルーティング
