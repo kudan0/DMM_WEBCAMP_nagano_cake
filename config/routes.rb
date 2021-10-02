4e90684b5512fe5ea0c070d760623083a52fd0f7
kudan0 <7xxx.ccs@gmail.com>
2021-10-02 14:39:09 +0000

    [add]新規登録ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   scope module: :public do
 
     get "/" => "homes#top"
-    resources :homes, only:[:about]
+    get "/about" => "homes#about"
+    # resources :homes, only:[:about]
     resources :items, only:[:index, :show]
-    resources :customers, only:[:show, :edit, :update, :confirm, :destroy]
+    resources :customers, only:[:confirm, :destroy]
     resources :cart_items, only:[:index, :update, :destroy, :destroy_all, :create]
     resources :orders, only:[:new, :confirm, :complete, :create, :index, :show]
     resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+    get "/customers/mypage" => "customers#show"
+    get "/customers/mypage/edit" => "customers#edit"
+    patch "/customers/mypage" => "customers#update"
+
   end
 
   # deviseのルーティングをadminsからadminへ変更
@@ -19,12 +24,23 @@ Rails.application.routes.draw do
         :passwords => 'devise/passwords'
       }
   # devise のルーティングをcustomersからpublicへ変更
-  devise_for :customers
+  # devise_for :customers, controllers: {
+  #   sessions: 'customers/sessions',
+  #   passwords: 'customers/passwords',
+  #   registrations: 'customers/registrations'
+  # }
+  devise_for :customers,
+    path: :customers,
+      :controllers => {
+        :registrations => 'devise/registrations',
+        :sessions => 'devise/sessions',
+        :passwords => 'devise/passwords'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 管理側ルーティング
   namespace :admin do
-    get "/" => "homes#top"
+    get "/admin" => "homes#top"
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
     resources :genres, only:[:index, :create, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
